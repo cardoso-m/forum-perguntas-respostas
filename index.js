059a53285e1b27cd717a1833c9d8aa93df6924ea
Matheus Cardoso <mdonizetecardoso@gmail.com>
2024-09-13 16:38:45 -0300

    Incluindo biblioteca body-parser para capturar os dados fornecidos no formulário

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const app = express()
+const bodyParser = require("body-parser")
 
 app.set("view engine", "ejs")
+
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
     res.render("index")
@@ -13,7 +17,9 @@ app.get("/pergunta", (req, res) => {
 })
 
 app.post("/perguntar", (req, res) => {
-    res.send("Ok")
+    var title = req.body.title
+    var desc = req.body.desc
+    res.send("Titulo:" + title + "<br>" + "Descrição:" + desc)
 })
 
 app.listen(8000, () => {
