d1371c0c9921b6e9239cff077daa55018a8d86a7
Matheus Cardoso <mdonizetecardoso@gmail.com>
2024-09-14 00:48:06 -0300

    Criando Modulo de resposta para a criação da tabela de respostas no banco e criação do frontend para responder

---
+++ database/resposta.js
@@ -0,0 +1,17 @@
+const Sequelize = require("sequelize")
+const connection = require("./database")
+
+const Resposta = connection.define('resposta', {
+    corpo: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    perguntaId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+})
+
+Resposta.sync({ force: false }).then(() => { })
+
+module.exports = Resposta
